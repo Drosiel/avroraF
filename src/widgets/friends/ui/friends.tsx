d98d4abe9f56231926f9172519ec3fcb46c1631b
Sergey Grimm <repisk@gmail.com>
2023-02-11 18:09:52 +0500

    изменил дизайн карточки друзей

--- src/widgets/friends/ui/friends.tsx
+++ src/widgets/friends/ui/friends.tsx
@@ -7,9 +7,19 @@ const Friends: FC = () => {
 
   return (
     <div>
-      <div>
+      <div className="p-1">
         {initiator?.friends?.map((friend) => (
-          <div key={friend.id}>
+          <div
+            key={friend.id}
+            className="flex items-center gap-2 py-2 px-4 text-lg bg-[#202025]"
+          >
+            {friend.logoURL && (
+              <img
+                className="w-14 h-14 object-cover rounded-full"
+                src={friend.logoURL}
+                alt="avatarF"
+              />
+            )}
             <p>{friend.name}</p>
           </div>
         ))}
