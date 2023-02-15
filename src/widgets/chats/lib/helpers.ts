f1358b37bd0394041834c04d615e19441a2f34b2
Sergey Grimm <repisk@gmail.com>
2023-02-15 18:01:41 +0500

    поправил запросы при вводе

--- src/widgets/chats/lib/helpers.ts
+++ src/widgets/chats/lib/helpers.ts
@@ -3,40 +3,37 @@ import { useSelector } from "react-redux";
 import { io } from "socket.io-client";
 import { RootState } from "../../../redux/store";
 
+const socket = io(
+  process.env.NODE_ENV === "development"
+    ? `ws://localhost:3001`
+    : `wss://avrorab-production.up.railway.app`
+);
 export const Notifications = () => {
-  const socket = io(
-    process.env.NODE_ENV === "development"
-      ? `ws://localhost:3001`
-      : `wss://avrorab-production.up.railway.app`
-  );
   const user = useSelector((state: RootState) => state.user.data);
 
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [dataValue, setDataValue] = useState();
 
   useEffect(() => {
-    if (user.name) {
-      socket.on("connect", () => {
-        setIsConnected(true);
-      });
+    socket.on("connect", () => {
+      setIsConnected(true);
+    });
 
-      socket.on("disconnect", () => {
-        setIsConnected(false);
-      });
-
-      socket.on("messageToClient", (data: any) => {
-        setDataValue(data);
-      });
-    }
-
-    if (!user.name) {
-      console.log("1234");
-      socket.disconnect();
-      socket.off("connect");
-      socket.off("disconnect");
-      socket.off("messageToClient");
+    socket.on("disconnect", () => {
       setIsConnected(false);
-    }
+    });
+
+    socket.on("messageToClient", (data: any) => {
+      setDataValue(data);
+    });
+
+    // if (!user.name) {
+    //   socket.disconnect();
+    //   socket.off("connect");
+    //   socket.off("disconnect");
+    //   socket.off("messageToClient");
+    //   setIsConnected(false);
+    // }
 
     return () => {
       socket.off("connect");
